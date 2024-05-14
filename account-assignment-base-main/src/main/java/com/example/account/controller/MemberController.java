97fb18a983605735ebcb09183678fcc17b183503
rtgc0915 <rtgc0915@gmail.com>
2024-05-14 15:09:37 +0900

    feat: create Login logic

---
+++ account-assignment-base-main/src/main/java/com/example/account/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.example.account.controller;
+
+import com.example.account.Service.MemberService;
+import com.example.account.dto.MemberRegisterDto;
+import com.example.account.util.response.CustomApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    //회원가입
+    @PostMapping("/sign-up")
+    public ResponseEntity<CustomApiResponse<?>> signUp(
+            @Valid @RequestBody MemberRegisterDto.Req req){
+        ResponseEntity<CustomApiResponse<?>> response = memberService.signUp(req);
+        return response;
+    }
+}
