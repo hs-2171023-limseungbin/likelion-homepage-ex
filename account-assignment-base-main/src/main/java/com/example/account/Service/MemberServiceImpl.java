6944f69f48f1bc8d7c7f8f5393332fc17a21433c
rtgc0915 <rtgc0915@gmail.com>
2024-05-14 15:09:46 +0900

    Revert "feat: create Login logic"
    This reverts commit 97fb18a983605735ebcb09183678fcc17b183503.

--- account-assignment-base-main/src/main/java/com/example/account/Service/MemberServiceImpl.java
+++ account-assignment-base-main/src/main/java/com/example/account/Service/MemberServiceImpl.java
@@ -1,13 +1,8 @@
 package com.example.account.Service;
 
 import com.example.account.Repository.MemberRepository;
-import com.example.account.domain.Member;
-import com.example.account.dto.MemberRegisterDto;
-import com.example.account.util.response.CustomApiResponse;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,17 +11,4 @@ import org.springframework.stereotype.Service;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
-    @Override
-    public ResponseEntity<CustomApiResponse<?>> signUp(MemberRegisterDto.Req req){
-        Member member = req.toEntity();
-
-        Member savedMember = memberRepository.save(member);
-
-        MemberRegisterDto.SignUp SignUpRes = new MemberRegisterDto.SignUp(savedMember.getId(),
-                savedMember.getUpdatedAt());
-        CustomApiResponse<MemberRegisterDto.SignUp> res = CustomApiResponse.createSuccess(HttpStatus.OK.value(),
-                SignUpRes, "회원가입에 성공하셨습니다.");
-        return ResponseEntity.ok(res);
-    }
 }
